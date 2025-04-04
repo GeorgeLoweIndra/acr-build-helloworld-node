e2dcd4ffa3eb7eda13ce2b278b43cfe248c60a96
GeorgeLoweIndra <106091818+GeorgeLoweIndra@users.noreply.github.com>
2025-04-04 10:52:08 +0100

    Update server.js 3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http')
 const port = 80
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'})
+  response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
   response.write('Hello World, Commit Successful!\n')
   response.write('⣿⣿⣿⣿⣿⣿⣿⡿⠟⠋⠉⢁⣀⣀⣀⡈⠉⠛⢿⡿⠿⢿⣿⣿⣿\n')
   response.write('⣿⣿⣿⣿⣿⣿⠏⢀⣴⣾⣿⣿⣿⣿⣿⡟⠃⢀⣀⣤⣤⣄⠉⢿⣿\n')
